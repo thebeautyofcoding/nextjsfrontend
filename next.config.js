fe8197614e2360be873c92afd7c7e576289b86a4
heiner <heiner.giehl@yahoo.com>
2021-02-24 18:59:41 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ module.exports = {
 
     publicRuntimeConfig: {
         APP_NAME: 'justPractice',
-        API_DEVELOPMENT: 'http://localhost:4000/api',
-        API_PRODUCTION: 'https://justpractice123.herokuapp.com/api',
-        PRODUCTION: false,
-        DOMAIN_DEVELOPMENT: 'https://localhost:4000',
-        DOMAIN_PRODUCTION: 'https://nextjsfrontend-ebon.vercel.app/',
-        GOOGLE_CLIENT_ID: '684060683436-qrcsake9ivp5gua1e3ikd7vn1sunkkae.apps.googleusercontent.com'
+        // API_DEVELOPMENT: 'http://localhost:4000/api',
+        // API_PRODUCTION: 'https://justpractice123.herokuapp.com/api',
+        // PRODUCTION: false,
+        // DOMAIN_DEVELOPMENT: 'https://localhost:4000',
+        // DOMAIN_PRODUCTION: 'https://nextjsfrontend-ebon.vercel.app/',
+        // GOOGLE_CLIENT_ID: '684060683436-qrcsake9ivp5gua1e3ikd7vn1sunkkae.apps.googleusercontent.com'
     }
 }
