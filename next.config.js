ab4bc29ba85e2c0424b76519da9d34411e6e4bde
heiner <heiner.giehl@yahoo.com>
2021-02-25 02:51:13 +0100

    ..

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
     publicRuntimeConfig: {
         DOMAIN_PRODUCTION: "https://nextjsfrontend-ebon.vercel.app",
-        API_PRODUCTION: "https://nextjsfrontend-ebon.vercel.app"
+        API_PRODUCTION: "https://nextjsfrontend-ebon.vercel.app/api"
 
     }
 }
