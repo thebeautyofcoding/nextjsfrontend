ec8adff7ba73daccb695ff096e584405d4e816b9
heiner <heiner.giehl@yahoo.com>
2021-02-25 03:34:53 +0100

    ..

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-
-
 import getConfig from 'next/config'
 
+const { publicRuntimeConfig } = getConfig()
 
 
-export const API = process.env.PRODUCTION ? process.env.API_PRODUCTION : process.env.API_DEVELOPMENT
+export const API = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.API_PRODUCTION : publicRuntimeConfig.API_DEVELOPMENT
 
-export const APP_NAME = process.env.APP_NAME
+export const APP_NAME = publicRuntimeConfig.APP_NAME
 
-export const DOMAIN = process.env.PRODUCTION ? process.env.DOMAIN_PRODUCTION : process.env.DOMAIN_DEVELOPMENT
+export const DOMAIN = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.DOMAIN_PRODUCTION : publicRuntimeConfig.DOMAIN_DEVELOPMENT
 
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+export const GOOGLE_CLIENT_ID = publicRuntimeConfig.GOOGLE_CLIENT_ID
