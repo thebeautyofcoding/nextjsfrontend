e52577a5d5045ec59468453973c92069b50347d6
heiner <heiner.giehl@yahoo.com>
2021-02-25 04:07:27 +0100

    ..

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ module.exports = {
         PRODUCTION:true,
         // DOMAIN_PRODUCTION: "https://nextjsfrontend2.vercel.app",
         API_PRODUCTION: "https://justpractice123.herokuapp.com/api",
-        APP_NAME: "justPractice"
+        APP_NAME: "justPractice",
+        GOOGLE_CLIENT_ID: '684060683436-qrcsake9ivp5gua1e3ikd7vn1sunkkae.apps.googleusercontent.com'
 
     }
 }
