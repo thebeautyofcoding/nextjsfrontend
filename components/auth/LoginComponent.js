d49cdee8cc2230abc41c7f7e5e334996ddb75a21
heiner <heiner.giehl@yahoo.com>
2021-02-25 11:31:44 +0100

    ..

--- components/auth/LoginComponent.js
+++ components/auth/LoginComponent.js
@@ -45,7 +45,7 @@ const router = useRouter()
            
                  axios({
                     method: 'post',
-                    url: process.env.API_PRODUCTION,
+                    url: process.env.API_PRODUCTION/signin,
                     data: bodyFormData,
                     headers: {'Content-Type': 'multipart/form-data' }
                  }).then((data) => {
