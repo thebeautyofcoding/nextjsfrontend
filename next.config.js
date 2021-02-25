9529fc1a7b9d12b9d7f0b3d479331cfc983e632e
heiner <heiner.giehl@yahoo.com>
2021-02-25 04:33:21 +0100

    ..

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-    publicRuntimeConfig: {
+    env: {
         PRODUCTION:true,
         DOMAIN_PRODUCTION: "https://nextjsfrontend2.vercel.app",
         API_PRODUCTION: "https://justpractice123.herokuapp.com/api",
