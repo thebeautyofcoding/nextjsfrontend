477240c9f7efad5c4f99aed9125431bf99bc32ec
heiner <heiner.giehl@yahoo.com>
2021-02-25 04:16:41 +0100

    ..

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
     publicRuntimeConfig: {
         PRODUCTION:true,
-        DOMAIN_PRODUCTION: "https://nextjsfrontend2-7wymi338w-thebeautyofcoding.vercel.app/",
+        DOMAIN_PRODUCTION: "https://nextjsfrontend2.vercel.app",
         API_PRODUCTION: "https://justpractice123.herokuapp.com/api",
         APP_NAME: "justPractice",
         GOOGLE_CLIENT_ID: '684060683436-qrcsake9ivp5gua1e3ikd7vn1sunkkae.apps.googleusercontent.com'
